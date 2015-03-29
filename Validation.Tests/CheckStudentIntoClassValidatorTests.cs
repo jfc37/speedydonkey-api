c08f6901cb3ff8571bbb0b93210db1f52e1e5bff
jfc37 <placid.joe@gmail.com>
2015-03-29 17:27:09 +1200

    Validation for class attendance

---
+++ Validation.Tests/CheckStudentIntoClassValidatorTests.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Data.Tests.Builders;
+using Models;
+using NUnit.Framework;
+using Validation.Validators;
+
+namespace Validation.Tests
+{
+    [TestFixture]
+    public class CheckStudentIntoClassValidatorTests
+    {
+        private MockRepositoryBuilder<Class> _classRepositoryBuilder;
+        private MockRepositoryBuilder<User> _userRepositoryBuilder;
+        private Class _class;
+        private User _retrievedUser;
+        private Class _retrievedClass;
+
+        private CheckStudentIntoClassValidator GetValidator()
+        {
+            return new CheckStudentIntoClassValidator(
+                _userRepositoryBuilder.BuildObject(),
+                _classRepositoryBuilder.BuildObject());
+        }
+
+        private FluentValidation.Results.ValidationResult PerforAction()
+        {
+            return GetValidator().Validate(_class);
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _retrievedClass = new Class
+            {
+                ActualStudents = new List<IUser>()
+            };
+            _classRepositoryBuilder = new MockRepositoryBuilder<Class>()
+                .WithGet(_retrievedClass);
+            _retrievedUser = new User
+            {
+                Passes = new List<IPass>
+                {
+                    new Pass
+                    {
+                        StartDate = DateTime.Now.AddDays(-1),
+                        EndDate = DateTime.Now.AddDays(1),
+                    }
+                }
+            };
+            _userRepositoryBuilder = new MockRepositoryBuilder<User>()
+                .WithGet(_retrievedUser);
+            _class = new Class
+            {
+                Id = 12,
+                ActualStudents = new List<IUser>
+                {
+                    new User()
+                }
+            };
+        }
+
+        [Test]
+        public void When_everything_is_valid_it_should_return_no_errors()
+        {
+            var result = PerforAction();
+
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [Test]
+        public void When_class_doesnt_exist_then_it_should_return_an_error()
+        {
+            _classRepositoryBuilder.WithUnsuccessfulGet();
+
+            var result = PerforAction();
+
+            Assert.IsFalse(result.IsValid);
+            var error = result.Errors.Single();
+            Assert.AreEqual(ValidationMessages.InvalidClass, error.ErrorMessage);
+        }
+        [Test]
+        public void When_multiple_users_being_added_then_it_should_return_an_error()
+        {
+            _class.ActualStudents.Add(new User());
+
+            var result = PerforAction();
+
+            Assert.IsFalse(result.IsValid);
+            var error = result.Errors.Single();
+            Assert.AreEqual(ValidationMessages.IncorrectNumberOfAttendees, error.ErrorMessage);
+        }
+        [Test]
+        public void When_user_doesnt_exist_then_it_should_return_an_error()
+        {
+            _userRepositoryBuilder.WithUnsuccessfulGet();
+
+            var result = PerforAction();
+
+            Assert.IsFalse(result.IsValid);
+            var error = result.Errors.Single();
+            Assert.AreEqual(ValidationMessages.InvalidUser, error.ErrorMessage);
+        }
+        [Test]
+        public void When_user_doesnt_have_a_valid_pass_then_it_should_return_an_error()
+        {
+            _userRepositoryBuilder.WithSuccessfulGet();
+
+            var result = PerforAction();
+
+            Assert.IsFalse(result.IsValid);
+            var error = result.Errors.Single();
+            Assert.AreEqual(ValidationMessages.NoValidPasses, error.ErrorMessage);
+        }
+        [Test]
+        public void When_user_is_already_attending_the_class_then_it_should_return_an_error()
+        {
+            _retrievedClass.ActualStudents.Add(_retrievedUser);
+
+            var result = PerforAction();
+
+            Assert.IsFalse(result.IsValid);
+            var error = result.Errors.Single();
+            Assert.AreEqual(ValidationMessages.AlreadyAttendingClass, error.ErrorMessage);
+        }
+    }
+}
--- Validation/ValidationMessages.cs
+++ Validation/ValidationMessages.cs
@@ -4,5 +4,10 @@
     {
         public const string AlreadyEnroledInBlock = "You are already enroled in a selected block";
         public const string InvalidBlock = "You are trying to enrol in a block that doesn't exist";
+        public const string InvalidClass = "You are trying to attend a class that doesn't exist";
+        public const string InvalidUser = "User does not exist";
+        public const string IncorrectNumberOfAttendees = "You can only add attendance for one student at a time";
+        public const string NoValidPasses = "Please purchase a pass before attending the class";
+        public const string AlreadyAttendingClass = "You are already attending this class";
     }
 }
