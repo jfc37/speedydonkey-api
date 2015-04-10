4fdb3d93b6b0adf4cf8604cbfe22f1bd6d8e90aa
jfc37 <placid.joe@gmail.com>
2015-04-10 18:06:52 +1200

    Added a bunch of configuration

--- Data.Tests/Builders/MockBuilders/MockAppSettingsBuilder.cs
+++ Data.Tests/Builders/MockBuilders/MockAppSettingsBuilder.cs
@@ -1,5 +1,6 @@
 ﻿using Common;
 using Common.Tests.Builders.MockBuilders;
+using Moq;
 
 namespace Data.Tests.Builders.MockBuilders
 {
@@ -9,6 +10,15 @@ namespace Data.Tests.Builders.MockBuilders
         {
             Mock.Setup(x => x.GetWebsiteUrl())
                 .Returns("https://something.com");
+            Mock.Setup(x => x.GetSetting(It.IsAny<AppSettingKey>()))
+                .Returns("blah x2");
+            return this;
+        }
+
+        public MockAppSettingsBuilder WithSetting(AppSettingKey appSettingKey, string value)
+        {
+            Mock.Setup(x => x.GetSetting(appSettingKey))
+                .Returns(value);
             return this;
         }
     }
