29393499e2ad0351630be6409dd68bf49df62fe0
jfc37 <placid.joe@gmail.com>
2015-09-17 21:15:40 +1200

    Fixing class roll

--- StudyBuddyApi/Controllers/ClassRollApiController.cs
+++ StudyBuddyApi/Controllers/ClassRollApiController.cs
@@ -1,17 +1,32 @@
+using System.Web.Http;
 using ActionHandlers;
-using Common;
+using Common.Extensions;
 using Data.Repositories;
 using Models;
+using SpeedyDonkeyApi.Filter;
+using SpeedyDonkeyApi.Models;
 
 namespace SpeedyDonkeyApi.Controllers
 {
     public class ClassRollApiController : EntityPropertyApiController
     {
+        private readonly IRepository<Class> _entityRepository;
+
         public ClassRollApiController(
             IRepository<Class> entityRepository,
             IActionHandlerOverlord actionHandlerOverlord)
             : base(actionHandlerOverlord)
         {
+            _entityRepository = entityRepository;
+        }
+
+        [ClaimsAuthorise(Claim = Claim.Teacher)]
+        public IHttpActionResult Get(int id)
+        {
+            var entity = _entityRepository.Get(id);
+            return entity.IsNotNull()
+                ? (IHttpActionResult)Ok(new ClassRegisterModel().ConvertFromEntity(entity))
+                : NotFound();
         }
     }
 }
