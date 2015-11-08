fa3c51789110f604fd14f2638c798dd9f4d37941
jfc37 <placid.joe@gmail.com>
2015-11-08 17:38:46 +1200

    Removed filters being added as global

--- StudyBuddyApi/App_Start/FilterConfig.cs
+++ StudyBuddyApi/App_Start/FilterConfig.cs
@@ -1,6 +1,5 @@
 using System.Web.Http;
 using System.Web.Mvc;
-using SpeedyDonkeyApi.Filter;
 
 namespace SpeedyDonkeyApi
 {
@@ -9,11 +8,6 @@ namespace SpeedyDonkeyApi
         public static void Register(HttpConfiguration config)
         {
             GlobalFilters.Filters.Add(new HandleErrorAttribute());
-
-            config.Filters.Add(new NullModelActionFilter());
-            config.Filters.Add(new ValidateModelActionFilter());
-            config.Filters.Add(new CurrentUserAttribute());
-            config.Filters.Add(new BasicAuthAuthoriseAttribute());
         }
     }
 }
