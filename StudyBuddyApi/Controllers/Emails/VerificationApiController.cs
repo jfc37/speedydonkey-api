754a0a690164d119c1097cdf0613eb93a6d79946
jfc37 <placid.joe@gmail.com>
2016-03-07 22:49:48 +1200

    Adding email verification end point

---
+++ StudyBuddyApi/Controllers/Emails/VerificationApiController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace SpeedyDonkeyApi.Controllers.Emails
+{
+    [RoutePrefix("api/emails/verification")]
+    public class VerificationApiController : ApiController
+    {
+        [Route]
+        public IHttpActionResult Post(object user)
+        {
+            return Ok();
+        }
+    }
+}
