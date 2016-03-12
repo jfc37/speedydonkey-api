423f9bc6c665662f0d4977915ccf9399d8e5c5c4
jfc37 <placid.joe@gmail.com>
2016-03-13 10:18:48 +1200

    Change enrolment template

--- Notification/Notifications/UserEnroledInBlock.cs
+++ Notification/Notifications/UserEnroledInBlock.cs
@@ -1,17 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Models;
 
 namespace Notification.Notifications
 {
     public class UserEnroledInBlock : INotification
     {
-        public string EmailTo { get; private set; }
-        public string Subject { get { return "Block Enrolment Confirmation"; } }
+        public string EmailTo { get; }
+        public string Subject => "Block Enrolment Confirmation";
         public string EmailBody { get; set; }
-        public string TemplateName { get { return "Block Enrolment"; } }
+        public string TemplateName => "Block Enrolment v2";
         public IList<KeyValuePair<string, string>> TemplateContent { get; set; }
         User User { get; set; }
 
